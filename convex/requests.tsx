2a67dcfc0eaeb4010a4027bb899294657966cfd5
Souvik Ghosh <mesouvikghosh96@gmail.com>
2025-07-28 22:00:06 +0530

    minor bug fix

--- convex/requests.tsx
+++ convex/requests.tsx
@@ -1,59 +1,143 @@
+// import { ConvexError } from "convex/values";
+// import { query } from "./_generated/server";
+// import { getUserByClerkId } from "./_utils";
+
+// export const get = query({
+//     args: {},
+//     handler: async (ctx, args) => {
+//         const identity = await ctx.auth.getUserIdentity()
+
+//         if (!identity) {
+//             throw new Error("Unauthorized access")
+//         }
+
+//         const currentUser = await getUserByClerkId({
+//             ctx,
+//             clerkId: identity.subject,
+//         })
+
+//         if (!currentUser) {
+//             throw new ConvexError("User not found")
+//         }
+
+//         const requests = await ctx.db.query("requests").withIndex("by_receiver", q => q.eq("receiver", currentUser._id)).collect()
+
+//         const requestWithSender = await Promise.all(requests.map(async requests => {
+//             const sender = await ctx.db.get(requests.sender)
+
+//             if (!sender) {
+//                 throw new ConvexError("Request sender could not be found")
+//             }
+
+//             return { sender, requests }
+//         }))
+//         return requestWithSender
+//     }
+// })
+
+// export const count = query({
+//     args: {},
+//     handler: async (ctx, args) => {
+//         const identity = await ctx.auth.getUserIdentity()
+
+//         if (!identity) {
+//             throw new Error("Unauthorized access")
+//         }
+
+//         const currentUser = await getUserByClerkId({
+//             ctx,
+//             clerkId: identity.subject,
+//         })
+
+//         if (!currentUser) {
+//             throw new ConvexError("User not found")
+//         }
+
+//         const requests = await ctx.db.query("requests").withIndex("by_receiver", q => q.eq("receiver", currentUser._id)).collect()
+//         return requests.length
+//     }
+// })
+
 import { ConvexError } from "convex/values";
 import { query } from "./_generated/server";
 import { getUserByClerkId } from "./_utils";
 
+// Helper function to get or create user
+async function getOrCreateUser(ctx: any, identity: any) {
+    let currentUser = await getUserByClerkId({
+        ctx,
+        clerkId: identity.subject,
+    });
+
+    // If user doesn't exist, create them
+    if (!currentUser) {
+        const userId = await ctx.db.insert("users", {
+            clerkId: identity.subject,
+            email: identity.email || "",
+            name: identity.name || "",
+            imageUrl: identity.pictureUrl || "",
+        });
+        
+        currentUser = await ctx.db.get(userId);
+    }
+
+    return currentUser;
+}
+
 export const get = query({
     args: {},
     handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity()
+        const identity = await ctx.auth.getUserIdentity();
 
         if (!identity) {
-            throw new Error("Unauthorized access")
+            throw new Error("Unauthorized access");
         }
 
-        const currentUser = await getUserByClerkId({
-            ctx,
-            clerkId: identity.subject,
-        })
+        const currentUser = await getOrCreateUser(ctx, identity);
 
         if (!currentUser) {
-            throw new ConvexError("User not found")
+            throw new ConvexError("Unable to create or find user");
         }
 
-        const requests = await ctx.db.query("requests").withIndex("by_receiver", q => q.eq("receiver", currentUser._id)).collect()
+        const requests = await ctx.db
+            .query("requests")
+            .withIndex("by_receiver", q => q.eq("receiver", currentUser._id))
+            .collect();
 
-        const requestWithSender = await Promise.all(requests.map(async requests => {
-            const sender = await ctx.db.get(requests.sender)
+        const requestWithSender = await Promise.all(requests.map(async request => {
+            const sender = await ctx.db.get(request.sender);
 
             if (!sender) {
-                throw new ConvexError("Request sender could not be found")
+                throw new ConvexError("Request sender could not be found");
             }
 
-            return { sender, requests }
-        }))
-        return requestWithSender
+            return { sender, request }; // Fixed: was 'requests' should be 'request'
+        }));
+
+        return requestWithSender;
     }
-})
+});
 
 export const count = query({
     args: {},
     handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity()
+        const identity = await ctx.auth.getUserIdentity();
 
         if (!identity) {
-            throw new Error("Unauthorized access")
+            throw new Error("Unauthorized access");
         }
 
-        const currentUser = await getUserByClerkId({
-            ctx,
-            clerkId: identity.subject,
-        })
+        const currentUser = await getOrCreateUser(ctx, identity);
 
         if (!currentUser) {
-            throw new ConvexError("User not found")
+            throw new ConvexError("Unable to create or find user");
         }
 
-        const requests = await ctx.db.query("requests").withIndex("by_receiver", q => q.eq("receiver", currentUser._id)).collect()
-        return requests.length
+        const requests = await ctx.db
+            .query("requests")
+            .withIndex("by_receiver", q => q.eq("receiver", currentUser._id))
+            .collect();
+
+        return requests.length;
     }
-})
+});
